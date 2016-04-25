6e66b089f3188434fd58ef3f3ff2f6851dcc3639
EmersonBSouza <emerson_bsouza@outlook.com>
2016-04-24 22:46:04 -0300

    Testes Prontos

--- Vruuummm Rotas/src/test/br/uefs/vrum/controller/ControllerTeste.java
+++ Vruuummm Rotas/src/test/br/uefs/vrum/controller/ControllerTeste.java
@@ -51,17 +51,80 @@ public class ControllerTeste {
 	@Test
 	public void recuperarVerticeSucesso(){
 		
+		Vertice vertice = CriarObjetos.criaVertice("Alameda Slim");
+		controller.adicionarVertice("Alameda Slim");
+		
+		try{
+			controller.recuperarVertice("Alameda Slim");
+		}catch(verticeInexistenteException e){
+			fail();
+		}
 	}
 	@Test
 	public void recuperarArestaSucesso(){
 		
+		Vertice origem = CriarObjetos.criaVertice("Aurora");
+		Vertice destino = CriarObjetos.criaVertice("Boreal");
+		Aresta aresta = CriarObjetos.criaAresta(origem, destino, 10);
+		
+		controller.adicionarVertice("Aurora");
+		controller.adicionarVertice("Boreal");
+		controller.adicionarAresta(origem, destino, 10);
+		
+		try {
+			String recebida = controller.recuperarAresta("Aurora","Boreal").getOrigem().getIndice();
+			String esperada = aresta.getOrigem().getIndice();
+			assertEquals(esperada,recebida);
+		} catch (verticeInexistenteException | arestaInexistenteException e) {
+			fail();
+		}
 	}
 	@Test
 	public void removerVerticeSucesso(){
 		
+		controller.adicionarVertice("Alameda Slim");
+		try{
+			controller.recuperarVertice("Alameda Slim");
+		}catch(verticeInexistenteException e){
+			fail();
+		}
+		try {
+			controller.removerVertice("Alameda Slim");
+		} catch (verticeInexistenteException e) {
+			fail();
+		}
+		try {
+			controller.recuperarVertice("Alameda Slim");
+		} catch (verticeInexistenteException e) {
+			assertTrue(true);
+		}
 	}
 	@Test
 	public void removerArestaSucesso(){
 		
+		Vertice origem = CriarObjetos.criaVertice("Aurora");
+		Vertice destino = CriarObjetos.criaVertice("Boreal");
+		Aresta aresta = CriarObjetos.criaAresta(origem, destino, 10);
+		
+		controller.adicionarVertice("Aurora");
+		controller.adicionarVertice("Boreal");
+		controller.adicionarAresta(origem, destino, 10);
+		
+		try {
+			String recebida = controller.recuperarAresta("Aurora","Boreal").getOrigem().getIndice();
+			String esperada = aresta.getOrigem().getIndice();
+		} catch (verticeInexistenteException | arestaInexistenteException e) {
+			fail();
+		}
+		try {
+			controller.removerAresta("Aurora","Boreal");
+		} catch (verticeInexistenteException | arestaInexistenteException e) {
+			fail();
+		}
+		try {
+			controller.recuperarAresta("Aurora","Boreal");
+		} catch (verticeInexistenteException | arestaInexistenteException e) {
+			assertTrue(true);
+		}
 	}
 }
