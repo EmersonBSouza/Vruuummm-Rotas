9a606a61f32ca092853b944bfcd1b654370f9a66
EmersonBSouza <emerson_bsouza@outlook.com>
2016-04-27 15:43:42 -0300

    Evento funcionando

--- Vruuummm Rotas/src/main/br/uefs/vrum/view/TelaPrincipal.java
+++ Vruuummm Rotas/src/main/br/uefs/vrum/view/TelaPrincipal.java
@@ -1,6 +1,9 @@
 package br.uefs.vrum.view;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.MouseAdapter;
@@ -9,6 +12,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.print.DocFlavor.URL;
+import javax.swing.ImageIcon;
 import javax.swing.JApplet;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -37,7 +42,6 @@ public class TelaPrincipal extends JApplet {
 		panel.setBounds(10, 11, 1120, 711);
 		getContentPane().add(panel);
 		
-		
 		panel_1.setBounds(1133, 11, 211, 711);
 		getContentPane().add(panel_1);
 		setSize(Toolkit.getDefaultToolkit().getScreenSize().width-10,Toolkit.getDefaultToolkit().getScreenSize().height-10);
@@ -46,6 +50,25 @@ public class TelaPrincipal extends JApplet {
 	public void adicionarPontoTela(int x,int y){
 		
 		coordenadas.salvarCoordenadas(x, y);
-		panel.add(coordenadas.getListaCoordenadas().get(coordenadas.getListaCoordenadas().size()-1));
+		repaint();
+	}
+	
+	public void paint(Graphics g){
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setColor(Color.black);
+
+		Iterator<JLabel> iterador = coordenadas.getListaCoordenadas().iterator();
+		java.net.URL url = null;
+		JLabel atual;
+		while(iterador.hasNext()) {
+			Font myFont = new Font ("Comic Sans", 1, 50);
+			g2d.setFont(myFont);
+			atual = (JLabel) iterador.next();
+			url = this.getClass().getResource("icone_Ponto.png");
+			g2d.drawImage(new ImageIcon(url).getImage(), atual.getX(), atual.getY(), null);
+			
+			g2d.drawString(atual.getText(), atual.getX()+15, atual.getY()+60);
+		}
 	}
 }
